b6e226a62f0b58158da4164a39ce8544302d76a8
Embers-of-the-Fire <stellarishs@163.com>
2025-08-03 11:12:29 +0800

    rm: remove unused format script

--- scripts/format.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-clang-format -i main.cpp
-
-find src/ -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i {} +
-
-find inc/ -type f -name "*.h" -exec clang-format -i {} +
